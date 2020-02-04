91ff3bfb5f192bb41a6eedb3686ca9a6449e12f1
Alisa Khaustova <alice.khaustova@gmail.com>
2020-02-04 19:06:00 +0200

    add async fetch

--- 09_lesson_final_work/main.js
+++ 09_lesson_final_work/main.js
@@ -1,158 +1,8 @@
-const arr = [
-    {
-        "login": "tr",
-        "id": 47003210,
-        "node_id": "MDEyOk9yZ2FuaXphdGlvbjQ3MDAzMjEw",
-        "avatar_url": "https://avatars3.githubusercontent.com/u/47003210?v=4",
-        "gravatar_id": "",
-        "url": "https://api.github.com/users/tr",
-        "html_url": "https://github.com/tr",
-        "followers_url": "https://api.github.com/users/tr/followers",
-        "following_url": "https://api.github.com/users/tr/following{/other_user}",
-        "gists_url": "https://api.github.com/users/tr/gists{/gist_id}",
-        "starred_url": "https://api.github.com/users/tr/starred{/owner}{/repo}",
-        "subscriptions_url": "https://api.github.com/users/tr/subscriptions",
-        "organizations_url": "https://api.github.com/users/tr/orgs",
-        "repos_url": "https://api.github.com/users/tr/repos",
-        "events_url": "https://api.github.com/users/tr/events{/privacy}",
-        "received_events_url": "https://api.github.com/users/tr/received_events",
-        "type": "Organization",
-        "site_admin": false,
-        "score": 135.70836
-      },
-      {
-        "login": "sineld",
-        "id": 445349,
-        "node_id": "MDQ6VXNlcjQ0NTM0OQ==",
-        "avatar_url": "https://avatars3.githubusercontent.com/u/445349?v=4",
-        "gravatar_id": "",
-        "url": "https://api.github.com/users/sineld",
-        "html_url": "https://github.com/sineld",
-        "followers_url": "https://api.github.com/users/sineld/followers",
-        "following_url": "https://api.github.com/users/sineld/following{/other_user}",
-        "gists_url": "https://api.github.com/users/sineld/gists{/gist_id}",
-        "starred_url": "https://api.github.com/users/sineld/starred{/owner}{/repo}",
-        "subscriptions_url": "https://api.github.com/users/sineld/subscriptions",
-        "organizations_url": "https://api.github.com/users/sineld/orgs",
-        "repos_url": "https://api.github.com/users/sineld/repos",
-        "events_url": "https://api.github.com/users/sineld/events{/privacy}",
-        "received_events_url": "https://api.github.com/users/sineld/received_events",
-        "type": "User",
-        "site_admin": false,
-        "score": 87.016495
-      },
-      {
-        "login": "Zuniceratops",
-        "id": 57756539,
-        "node_id": "MDQ6VXNlcjU3NzU2NTM5",
-        "avatar_url": "https://avatars2.githubusercontent.com/u/57756539?v=4",
-        "gravatar_id": "",
-        "url": "https://api.github.com/users/Zuniceratops",
-        "html_url": "https://github.com/Zuniceratops",
-        "followers_url": "https://api.github.com/users/Zuniceratops/followers",
-        "following_url": "https://api.github.com/users/Zuniceratops/following{/other_user}",
-        "gists_url": "https://api.github.com/users/Zuniceratops/gists{/gist_id}",
-        "starred_url": "https://api.github.com/users/Zuniceratops/starred{/owner}{/repo}",
-        "subscriptions_url": "https://api.github.com/users/Zuniceratops/subscriptions",
-        "organizations_url": "https://api.github.com/users/Zuniceratops/orgs",
-        "repos_url": "https://api.github.com/users/Zuniceratops/repos",
-        "events_url": "https://api.github.com/users/Zuniceratops/events{/privacy}",
-        "received_events_url": "https://api.github.com/users/Zuniceratops/received_events",
-        "type": "User",
-        "site_admin": false,
-        "score": 196.23973
-      },
-      {
-        "login": "Gekol",
-        "id": 24961389,
-        "node_id": "MDQ6VXNlcjI0OTYxMzg5",
-        "avatar_url": "https://avatars2.githubusercontent.com/u/24961389?v=4",
-        "gravatar_id": "",
-        "url": "https://api.github.com/users/Gekol",
-        "html_url": "https://github.com/Gekol",
-        "followers_url": "https://api.github.com/users/Gekol/followers",
-        "following_url": "https://api.github.com/users/Gekol/following{/other_user}",
-        "gists_url": "https://api.github.com/users/Gekol/gists{/gist_id}",
-        "starred_url": "https://api.github.com/users/Gekol/starred{/owner}{/repo}",
-        "subscriptions_url": "https://api.github.com/users/Gekol/subscriptions",
-        "organizations_url": "https://api.github.com/users/Gekol/orgs",
-        "repos_url": "https://api.github.com/users/Gekol/repos",
-        "events_url": "https://api.github.com/users/Gekol/events{/privacy}",
-        "received_events_url": "https://api.github.com/users/Gekol/received_events",
-        "type": "User",
-        "site_admin": false,
-        "score": 330.9392
-      },
-      {
-        "login": "KyslenkoAnna",
-        "id": 59174272,
-        "node_id": "MDQ6VXNlcjU5MTc0Mjcy",
-        "avatar_url": "https://avatars1.githubusercontent.com/u/59174272?v=4",
-        "gravatar_id": "",
-        "url": "https://api.github.com/users/KyslenkoAnna",
-        "html_url": "https://github.com/KyslenkoAnna",
-        "followers_url": "https://api.github.com/users/KyslenkoAnna/followers",
-        "following_url": "https://api.github.com/users/KyslenkoAnna/following{/other_user}",
-        "gists_url": "https://api.github.com/users/KyslenkoAnna/gists{/gist_id}",
-        "starred_url": "https://api.github.com/users/KyslenkoAnna/starred{/owner}{/repo}",
-        "subscriptions_url": "https://api.github.com/users/KyslenkoAnna/subscriptions",
-        "organizations_url": "https://api.github.com/users/KyslenkoAnna/orgs",
-        "repos_url": "https://api.github.com/users/KyslenkoAnna/repos",
-        "events_url": "https://api.github.com/users/KyslenkoAnna/events{/privacy}",
-        "received_events_url": "https://api.github.com/users/KyslenkoAnna/received_events",
-        "type": "User",
-        "site_admin": false,
-        "score": 145.64293
-      },
-      {
-        "login": "guniamaxim",
-        "id": 52348667,
-        "node_id": "MDQ6VXNlcjUyMzQ4NjY3",
-        "avatar_url": "https://avatars0.githubusercontent.com/u/52348667?v=4",
-        "gravatar_id": "",
-        "url": "https://api.github.com/users/guniamaxim",
-        "html_url": "https://github.com/guniamaxim",
-        "followers_url": "https://api.github.com/users/guniamaxim/followers",
-        "following_url": "https://api.github.com/users/guniamaxim/following{/other_user}",
-        "gists_url": "https://api.github.com/users/guniamaxim/gists{/gist_id}",
-        "starred_url": "https://api.github.com/users/guniamaxim/starred{/owner}{/repo}",
-        "subscriptions_url": "https://api.github.com/users/guniamaxim/subscriptions",
-        "organizations_url": "https://api.github.com/users/guniamaxim/orgs",
-        "repos_url": "https://api.github.com/users/guniamaxim/repos",
-        "events_url": "https://api.github.com/users/guniamaxim/events{/privacy}",
-        "received_events_url": "https://api.github.com/users/guniamaxim/received_events",
-        "type": "User",
-        "site_admin": false,
-        "score": 142.90515
-      },
-      {
-        "login": "archontis-cpu",
-        "id": 57407473,
-        "node_id": "MDQ6VXNlcjU3NDA3NDcz",
-        "avatar_url": "https://avatars3.githubusercontent.com/u/57407473?v=4",
-        "gravatar_id": "",
-        "url": "https://api.github.com/users/archontis-cpu",
-        "html_url": "https://github.com/archontis-cpu",
-        "followers_url": "https://api.github.com/users/archontis-cpu/followers",
-        "following_url": "https://api.github.com/users/archontis-cpu/following{/other_user}",
-        "gists_url": "https://api.github.com/users/archontis-cpu/gists{/gist_id}",
-        "starred_url": "https://api.github.com/users/archontis-cpu/starred{/owner}{/repo}",
-        "subscriptions_url": "https://api.github.com/users/archontis-cpu/subscriptions",
-        "organizations_url": "https://api.github.com/users/archontis-cpu/orgs",
-        "repos_url": "https://api.github.com/users/archontis-cpu/repos",
-        "events_url": "https://api.github.com/users/archontis-cpu/events{/privacy}",
-        "received_events_url": "https://api.github.com/users/archontis-cpu/received_events",
-        "type": "User",
-        "site_admin": false,
-        "score": 208.56337
-      },
-];
-
 let numBlock = document.querySelector('input');
-let userList = document.querySelector('.user-list');
 let cardList = document.querySelector('#card-list');
 
-numBlock.addEventListener('input', function(event) {
+
+numBlock.addEventListener('input', async function(event) {
     let value = numBlock.value;
 
     if (value === '') {
@@ -160,21 +10,26 @@ numBlock.addEventListener('input', function(event) {
         return;
     }
 
-    let arrName = arr.filter(function(elem) {
-        let name = elem.login.toLowerCase();
-        let index = name.indexOf(value);
+    // асинхронный запрос
+    const response = await fetch(`https://api.github.com/search/users?q=${value}`);
+    const data = await response.json();
+  
 
-        if (index >= 0) {
-            return true;
-        }
+    let arrName = data.items.filter(function(elem) {
+      let name = elem.login.toLowerCase();
+      let index = name.indexOf(value);
 
-        return false;
+      if (index >= 0) {
+          return true;
+      }
+
+      return false;
     })
     
     // Clear old results
     cardList.innerHTML = '';
 
-    // Add new
+    // Add new users cards
     arrName.forEach(function(elem) {
         const card = createCard(elem);
         cardList.appendChild(card);
