e0ca576a9776e41942bcfb781bd05536e0b94acd
Alisa Khaustova <alice.khaustova@gmail.com>
2020-02-05 23:02:11 +0200

    add spinner and warnind,try/catch

--- 09_lesson_final_work/main.js
+++ 09_lesson_final_work/main.js
@@ -1,27 +1,38 @@
 let numBlock = document.querySelector('input');
 let cardList = document.querySelector('#card-list');
+let spinner = document.querySelector('#spinner');
+let warning = document.querySelector('#warning');
 let timerId = null;
 
-numBlock.addEventListener('input', function(event) {
-  if(timerId) {
+
+numBlock.addEventListener('input', function() {
+  let value = numBlock.value;
+
+  if (timerId) {
     clearTimeout(timerId);
   };
 
-  let value = numBlock.value;
+//очищаем строку ввода
+  if (value === '') {
+    cardList.innerHTML = '';
+    return;
+  }
 
-    if (value === '') {
-        cardList.innerHTML = '';
-        return;
-    }
-
+  cardList.innerHTML = '';
+  //запускаем в работу спиннер, который был скрыт
+  spinner.classList.remove('d-none');
+  //перехват ошибки в работающем коде
+  warning.classList.add('d-none');
+ 
     timerId = setTimeout(async function() {
-    // асинхронный запрос
-      const response = await fetch(`https://api.github.com/search/users?q=${value}`);
-      const data = await response.json();
-    
-      let arrName = data.items.filter(function(elem) {
-        let name = elem.login.toLowerCase();
-        let index = name.indexOf(value);
+      // асинхронный запрос
+      try {
+         const response = await fetch(`https://1api.github.com/search/users?q=${value}`);
+        const data = await response.json();
+
+        let arrName = data.items.filter(function(elem) {
+          let name = elem.login.toLowerCase();
+          let index = name.indexOf(value);
 
           if (index >= 0) {
               return true;
@@ -29,15 +40,20 @@ numBlock.addEventListener('input', function(event) {
 
           return false;
         })
-        
-      // Clear old results
-      cardList.innerHTML = '';
+          
+        // Clear old results
+        cardList.innerHTML = '';
 
-      // Add new users cards
-      arrName.forEach(function(elem) {
+        // Add new users cards
+        arrName.forEach(function(elem) {
           const card = createCard(elem);
           cardList.appendChild(card);
-      });
+        });
+      } catch(error) {
+        warning.classList.remove('d-none');
+      } finally {
+        // spinner.classList.remove('d-none');
+      };
     }, 400);
 });
 
